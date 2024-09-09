681e0077bb14f8940b0b4fe919604bfbd37df676
조우식 <chousik01@gmail.com>
2024-09-09 12:55:23 +0900

    py syntax tutorial

---
+++ testOne.py
@@ -0,0 +1,36 @@
+a = "조우식"
+b = "남자"
+
+
+def join_string():
+    global a,b
+    return a + b
+  
+  
+print(join_string())
+
+
+
+def example():
+    return "test"
+
+
+anther_example = lambda: "테스트"
+
+print(anther_example())
+
+array = ["배성빈","문혜림","신지윤"]
+
+obj = {
+    "조자연" : "안녕",
+    "김보미" : "개굴",
+    "황재민" : "수면왕",
+    "황재민" : "수면왕",
+    "황재민" : "수면왕",
+    "황재민" : "수면왕",
+    "황재민" : "수면왕",
+    "황재민" : "수면왕",
+}
+
+
+another_obj = ("김정수","송이현","최유진")
