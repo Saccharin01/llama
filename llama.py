d3573058e978a143600efe19fabfbcca40a46d06
조우식 <chousik01@gmail.com>
2024-09-09 16:50:17 +0900

    llama.py done

--- llama.py
+++ llama.py
@@ -1,24 +1,35 @@
-import subprocess
+import ollama
+import json
+
 
 def main():
-    while True:
-        user_input = input("입력하세요 (종료하려면 'exit' 입력): ")
-
-        if user_input.lower() == 'exit':
-            break
-
-        command = [r"C:\Users\Administrator\AppData\Local\Programs\Ollama\ollama.exe", "run", "llama3.1", user_input]
-
-        try:
-            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
-            if result.stdout:
-                print("LLaMA의 응답: ", result.stdout)
-            if result.stderr:
-                print("오류 발생:", result.stderr)
-        except FileNotFoundError:
-            print("지정된 파일을 찾을 수 없습니다. 경로와 명령어를 확인해 보세요.")
-        except UnicodeDecodeError as e:
-            print("문자 인코딩 오류 발생:", e)
+  conversationLog = []
+  file_path = "test.json"
+  while True:
+      user_input = input("라마에게 뭐라고 물어볼래요? (종료하려면 'exit' 입력): ")
+      
+      if user_input.lower() == 'exit':
+        with open(file_path, "w", encoding="utf-8")as file:
+          json.dump(conversationLog, file, ensure_ascii=False, indent=2)
+          file.write(conversationLog)
+          break
+      
+      try:
+          response = ollama.chat(model='llama3.1', messages=[
+              {
+                  "role": "user",
+                  "content": user_input
+              }
+          ])
+          print("LLaMA의 응답:", response['message']['content'])
+          conversationLog.append({
+            "user_input" : user_input,
+            "response" : response["message"]['content'],
+            "create_date" : response["created_at"]
+          })
+          print(conversationLog)
+      except Exception as e:
+          print("오류 발생:", e)
 
 if __name__ == "__main__":
     main()
